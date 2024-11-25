c96ef20dbf767b1b0b8cf63c8708a6cde3359d98
Yukie765 <yliya-kate@mail.ru>
2024-11-26 00:44:14 +0500

    Update Module_11_2.py
    Add modules

--- Module_11_2.py
+++ Module_11_2.py
@@ -1,5 +1,4 @@
-from fontTools.misc.cython import returns
-
+import inspect
 
 def introspection_info(obj):
     res,_name,_class = '','',''
@@ -7,7 +6,7 @@ def introspection_info(obj):
     _type =f'type: {type(obj)}, '
     _methods = f'methods: {[x for x in _listinfo if '__' in x]}, '
     _attrs = f'attrs: {[x for x in _listinfo if '__' not in x]}, '
-
+    _module = f'module:  {inspect.getmodule(obj) if not None else ''}, '
     if hasattr(obj, '__name__'):
        _name = f'name {obj.__name__}, '
     if hasattr(obj, '__class__'):
@@ -15,7 +14,7 @@ def introspection_info(obj):
     _id = f'id: {id(obj)}, '
     _iscallable = f'callable: {True if callable(obj) else False}'
 
-    _res = _type + _methods + _attrs + _name + _class+ _id+_iscallable
+    _res = _type + _methods + _attrs + _module + _name + _class+ _id+_iscallable
     return _res
 
 
